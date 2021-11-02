bf46e88a0181a385a81092d5c1ad8b553b049200
sarveshhome <sarvesh.carlton@gmail.com>
2021-11-02 15:25:24 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  @ViewChild('input') vc: ElementRef;
+  name = 'Angular';
+
+  ngAfterViewInit() {
+    this.vc.nativeElement.focus();
+  }
+}
